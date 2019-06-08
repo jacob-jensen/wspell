39ea0025ca58f8ab148b4117dc4a9fc6f3359ff0
Jacob Jensen <jacob.jensen.spam@gmail.com>
2019-06-08 10:31:23 +0200

    Skeleton for mux and json

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 )
 
+//Word contains a single word
+type Word struct {
+	Word        string `json:"word"`
+	Category    int    `json:"category"`
+	PicturePath string `json:"picturePath"`
+}
+
 func main() {
-	fmt.Println("hello world")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		words := []Word{
+			Word{Word: "hej", Category: 1},
+			Word{Word: "med dig", Category: 2},
+		}
+		if err := json.NewEncoder(w).Encode(words); err != nil {
+			panic(err)
+		}
+	})
+
+	http.ListenAndServe(":8080", mux)
 }
